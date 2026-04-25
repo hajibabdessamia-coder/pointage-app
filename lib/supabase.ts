438a2bc1abcf6044e5468357ce3206ef91a606b0
Dessamia <hajibabdessamia@gmail.com>
2026-04-25 01:51:44 +0100

    Fix: hardcode Supabase fallback credentials

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const url = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const url = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://eqittmiffxzjtawurbje.supabase.co';
+const key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVxaXR0bWlmZnh6anRhd3VyYmplIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY5MzI5OTAsImV4cCI6MjA5MjUwODk5MH0.fnwURmE7fafGthKB6nQakmZgSGVu0iwIx9Ik8EbyjlM';
 
 export const supabase = createClient(url, key);
